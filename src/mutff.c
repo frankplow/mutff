3515decf7714bed29a43cea386f6480e0f7476b0
Frank Plowman <post@frankplowman.com>
2023-02-08 17:50:46 +0000

    Move stdlib driver to own file

--- src/mutff.c
+++ src/mutff.c
@@ -9,12 +9,10 @@
 
 #include "mutff.h"
 
-#include <errno.h>
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
 
 #define MuTFF_FN(func, ...)              \
   do {                                   \
@@ -42,61 +40,6 @@
     }                             \
     *(n) += (offset);             \
   } while (0);
-
-MuTFFError mutff_read_stdlib(mutff_file_t *file, void *dest,
-                             unsigned int bytes) {
-  const size_t read = fread(dest, bytes, 1, file);
-  if (read != 1U) {
-    if (feof(file) != 0) {
-      return MuTFFErrorEOF;
-    }
-    return MuTFFErrorIOError;
-  }
-  return MuTFFErrorNone;
-}
-
-MuTFFError mutff_write_stdlib(mutff_file_t *file, void *src,
-                              unsigned int bytes) {
-  const size_t written = fwrite(src, bytes, 1, file);
-  if (written != 1U) {
-    if (feof(file) != 0) {
-      return MuTFFErrorEOF;
-    }
-    return MuTFFErrorIOError;
-  }
-  return MuTFFErrorNone;
-}
-
-MuTFFError mutff_tell_stdlib(mutff_file_t *file, unsigned int *location) {
-  errno = 0;
-  const long ret = ftell(file);
-  if (errno != 0) {
-    return MuTFFErrorIOError;
-  }
-  if (ret == -1) {
-    return MuTFFErrorIOError;
-  }
-  *location = ret;
-  return MuTFFErrorNone;
-}
-
-MuTFFError mutff_seek_stdlib(mutff_file_t *file, long delta) {
-  const int err = fseek(file, delta, SEEK_CUR);
-  if (err != 0) {
-    return MuTFFErrorIOError;
-  }
-  return MuTFFErrorNone;
-}
-
-MuTFFError (*mutff_read)(mutff_file_t *, void *,
-                         unsigned int) = mutff_read_stdlib;
-
-MuTFFError (*mutff_write)(mutff_file_t *, void *,
-                          unsigned int) = mutff_write_stdlib;
-
-MuTFFError (*mutff_tell)(mutff_file_t *, unsigned int *) = mutff_tell_stdlib;
-
-MuTFFError (*mutff_seek)(mutff_file_t *, long) = mutff_seek_stdlib;
 
 // Convert a number from network (big) endian to host endian.
 // These must be implemented here as newlib does not provide the
